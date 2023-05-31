95f7528f5df5d8ec1b0ba72f4bba7e382cb68c35
Artem <artem33433@yandex.ru>
2023-05-31 07:25:26 +0300

    L2T3

---
+++ Lesson2/Task3/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <fstream>
+#include <iostream>
+
+
+struct info {
+    std::string city;
+    std::string street;
+    int House;
+    int Num;
+    std::string Ind;
+};
+
+void Console(info a) {
+    std::cout << std::endl << "Город: "<< a.city << std::endl << "Улица: " << a.street << std::endl << "Номер дома: " << a.House << std::endl << "Номер квартиры: " << a.Num << std::endl << "Индекс: " << a.Ind << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    info Inf;
+
+    info P = { "gorod","street",'2','1',"232090" };
+    Console(P);
+   
+    std::string G;
+    std::string U;
+    std::string I;
+    int H,K;
+    std::cout<<std::endl;
+    std::cout << "Введите город: ";
+    std::cin >> G;
+    std::cout << "Введите улицу: ";
+    std::cin >> U;
+    std::cout << "Введите номер дома: ";
+    std::cin >> H;
+    std::cout << "Введите номер квартиры: ";
+    std::cin >> K;
+    std::cout << "Введите индекс: ";
+    std::cin >> I;
+
+    info L = { G,U,H,K,I };
+    Console(L);
+  
+}
+
