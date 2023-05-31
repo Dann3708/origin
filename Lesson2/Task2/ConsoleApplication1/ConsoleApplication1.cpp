388ccbc2c7aa384d1147a3bed00ac25372087ab1
Artem <artem33433@yandex.ru>
2023-05-31 06:37:37 +0300

    L2T2

---
+++ Lesson2/Task2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+
+struct person {
+    std::string Num;
+    std::string Name;
+    int summ;
+
+};
+
+void change_summ(person* p, int x) {
+    p->summ = x;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int summ2;
+    person pers;
+    std::cout << "Введите номер счёта: ";
+    std::cin >> pers.Num;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> pers.Name;
+    std::cout <<"Введите баланс: ";
+    std::cin >> pers.summ;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> summ2;
+    change_summ(&pers, summ2);
+    std::cout << "Ваш счёт: "<<pers.Name<<", "<<pers.Num<<", "<< pers.summ;
+}
+
